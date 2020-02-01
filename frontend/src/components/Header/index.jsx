b3a1f7c99a6ea6dd1f061dbb65a5d61d46433792
Matteo Fusilli <matteofusilli@Matteos-MacBook-Pro.local>
2020-02-01 17:45:34 +0000

    finalised appbar with auth0

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,45 +1,160 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from '../../contexts/auth0-context';
-import './Header.css'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from '@material-ui/core/Avatar';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  userAvatar: {
+    marginLeft: 20,
+    marginRight: 20
+  }
+}));
 
 export default function Header() {
+  const classes = useStyles();
   const { isLoading, user, loginWithRedirect, logout } = useAuth0();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = event => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id='primary-search-account-menu'
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id='primary-search-account-menu-mobile'
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
-    <header>
-      <nav className="navbar is-dark">
-        <div className="container">
-          <div className="navbar-menu is-active">
-            {/* logo */}
-            <div className="navbar-brand">
-              <button className="navbar-item">My Cool App!</button>
-            </div>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Dashboard
+          </Typography>
+          {!isLoading && !user && (
+            <>
+              <Button onClick={loginWithRedirect} color="inherit">Login</Button>
+              <IconButton
+                edge="end"
+                aria-label="account of current user"
+                aria-controls='primary-search-account-menu'
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </>
+          )}
 
-            {/* menu items */}
-            <div className="navbar-end">
-              {/* if there is no user. show the login button */}
-              {!isLoading && !user && (
-                <button onClick={loginWithRedirect} className="navbar-item">
-                  Login
-                </button>
-              )}
+          {/* if there is a user. show user name and logout button */}
 
-              {/* if there is a user. show user name and logout button */}
-              {!isLoading && user && (
-                <>
-                  <button className="navbar-item">{user.name}</button>
-                  <button
-                    onClick={() => logout({ returnTo: window.location.origin })}
-                    className="navbar-item"
-                  >
-                    Logout
-                  </button>
-                </>
-              )}
-            </div>
-          </div>
-        </div>
-      </nav>
-    </header>
+          {!isLoading && user && (
+            <>
+              <Typography>{user.name}</Typography>
+              <Avatar className={classes.userAvatar} alt={user.name} src={user.picture} />
+              <Button
+                onClick={() => logout({ returnTo: window.location.origin })}
+                color="inherit">Logout</Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
   );
 }
+
