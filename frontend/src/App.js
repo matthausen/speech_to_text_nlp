81500055e9123ca7dc277be7016c3664532292e1
Matteo Fusilli <matteofusilli@Matteos-MacBook-Pro.local>
2020-02-01 16:46:46 +0000

    added auth0 authorization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
 import Main from './components/Main';
+import { useAuth0 } from './contexts/auth0-context';
 import { SnackbarProvider } from 'notistack';
+import Header from './components/Header';
 
 function App() {
+  const { isLoading, user, loginWithRedirect, logout } = useAuth0();
+
   return (
     <div className="App">
       <SnackbarProvider maxSnack={3}>
+        <Header />
         <Main />
+        {!isLoading && !user && (
+          <>
+            <h1>Click Below!</h1>
+            <button onClick={loginWithRedirect} className="button is-danger">
+              Login
+              </button>
+          </>
+        )}
+        {!isLoading && user && (
+          <>
+            <h1>You are logged in!</h1>
+            <p>Hello {user.name}</p>
+
+            {user.picture && <img src={user.picture} alt="My Avatar" />}
+            <hr />
+
+            <button
+              onClick={() => logout({ returnTo: window.location.origin })}
+              className="button is-small is-dark"
+            >
+              Logout
+                </button>
+          </>
+        )}
       </SnackbarProvider>
     </div>
   );
